ff01c85588f77c28f6a57ed6f0f519fdb8c42ee2
Urdhva <urdhva.borse@gmail.com>
2025-03-21 17:20:47 +0530

    updated header and main file

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 #endif
 
 #include <windows.h>
+#include <new>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lparam);
+#include "main.h"
+
+HMENU hMenu;
 
 //this is the program entry point 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
@@ -13,16 +16,18 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     WNDCLASS wc = {};
 
+    //this is a pointer to a function, thats why no parenthesis
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;      //the class name we defined
 
     RegisterClass(&wc);
 
-    //create the window
-
+    // StateInfo* pState = new (std::nothrow) StateInfo;
+    // if(!RegisterClassW(&wc))    return -1;      //if window fails to load, return -1
 
     //so this is a data structure apparently.
+    //keep this structure same for every window
     HWND hwnd = CreateWindowEx(
         0,      //window styles like transparency
         CLASS_NAME,         //window class namme i guess
@@ -44,17 +49,40 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     //run message loop
 
-    MSG msg = {};
+    //the get message loop keeps running to take input/messages from the user. 
+    //the function removes the first message at the head of the queue
+    MSG msg = {};       //this is actually a structure - handles all kinds of messages (keyboard and mouse)
     while(GetMessage(&msg, NULL, 0,0) > 0){
+        //takes messages from the keyboard and translates it. 
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     } 
+
+    return 0;
 }
 
+//this stands for window procedure
+//basically - wParam and lParam are defined based on uMsg.
+//uMsg tells us what message is being passed. wParam and lParam gives properties of that message
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    //this whole function is used as a switch case for *messages* from windows
     switch(uMsg){
+        case WM_COMMAND:
+            switch(wParam)
+            {
+            case 1:
+                MessageBox(hwnd, L"I love Old-Kimchi", L"Okay", 1);
+            }
+            return 0;
+        case WM_CREATE:
+        {
+            DefWindowProc(hwnd, uMsg, wParam, lParam);
+            AddMenus(hwnd);
+        }
+        return 0 ;       
         case WM_DESTROY:
+            //this is self explanatory
             PostQuitMessage(0);
             return 0;
 
@@ -68,10 +96,23 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 EndPaint(hwnd, &ps);
             }
             return 0;
+        default: 
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
+
+void AddMenus(HWND hwnd){
+    hMenu = CreateMenu();
+
+    //always check data type and fill in a random value when you want to test. 
+    AppendMenuW(hMenu, MF_STRING, 1, L"File");
+    AppendMenuW(hMenu, MF_STRING, 2, L"Edit");
+    AppendMenuW(hMenu, MF_STRING, 3, L"Selection");
+    AppendMenuW(hMenu, MF_STRING, 4, L"View");
+
+    SetMenu(hwnd, hMenu);
+}
+
 
 //windowProc defines the behavior of the window. How the window interacts
 //with the user and all
--- main.h
+++ main.h
@@ -1,62 +1,7 @@
 #pragma once
-
 #include <windows.h>
 
-#include <string>
-#include <functional>
+//header file for forward declerations
 
-namespace ichiji
-{
-    class Seika
-    {
-    private:
-        HINSTANCE hInstance;
-        HWND windowHandle;
-        bool running = false;
-
-        std::wstring windowTitle;
-        int windowHeight;
-        int windowWidth;
-
-        //user defined game update function
-        std::function<void(float delta)> update;
-
-        void startWindow();
-
-    public:
-        //all of this is to create a singleton instance - aka not more than once instance of the app is running
-        Seika(){};      //default constructor
-        Seika(const Seika&) = delete;
-        Seika& operator=(const Seika&) = delete;
-
-        ~Seika() {}
-
-        inline static Seika& getInstance(){
-            static Seika seika;
-            return seika;
-        }
-
-        inline static void start(){
-            getInstance().startWindow();
-        }
-
-        inline static void setGameUpdate(const std::function<void(float delta)>& update){
-            //update is a variable
-            getInstance().update = update;
-        }
-
-        inline std::wstring getWindowTitle(){return getInstance().windowTitle;}
-        inline int getWindowHeight(){return getInstance().windowHeight;}
-        inline int getWindowWidth(){return getInstance().windowWidth;}
-
-
-
-    };
-}
-
-ichiji::Seika startWindow(){
-    //don't use wchar - try with basic char first
-    const char* className = "ichijiWindow";
-    WNDCLASS windowClass = {};
-    
-}
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lparam);
+void AddMenus(HWND hwnd);
